3c3940692b4f7a7efe417b4b80c39269691e279f
NaNBar <saeedtqp@gmail.com>
2022-12-18 17:54:18 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-PORT = 443
+PORT = 420
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
-    # "tg2": "0123456789abcdef0123456789abcdef",
+    "tg":  "9d4ea9da397079266000cc26d6b30bd5",
 }
 
 MODES = {
@@ -21,7 +20,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "b306c7fb16359572a01e7a5732863036"
